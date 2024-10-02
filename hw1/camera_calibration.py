db392f9ed118ddfe5de3157bbc5d89eb097be822
LolaInTa Chi <lolainta@gmail.com>
2024-10-03 06:04:44 +0800

    init: hw1 done

---
+++ hw1/camera_calibration.py
@@ -0,0 +1,171 @@
+import numpy as np
+import cv2
+import glob
+import matplotlib.pyplot as plt
+import camera_calibration_show_extrinsics as show
+from icecream import ic
+from utils import compute_extrinsic_matrix, compute_homography, compute_intrinsic_matrix
+
+np.set_printoptions(suppress=True, linewidth=200, threshold=10)
+ic.configureOutput(includeContext=True)
+ic.disable()
+
+
+def cv2_example(img_size, imgpoints, objpoints):
+    # You need to comment these functions and write your calibration function from scratch.
+    # Notice that rvecs is rotation vector, not the rotation matrix, and tvecs is translation vector.
+    # In practice, you'll derive extrinsics matrixes directly. The shape must be [pts_num,3,4], and use them to plot.
+    ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(
+        objpoints, imgpoints, img_size, None, None
+    )
+    ic(ret, mtx, dist, rvecs, tvecs)
+    Vr = np.array(rvecs)
+    Tr = np.array(tvecs)
+    extrinsics = np.concatenate((Vr, Tr), axis=1).reshape(-1, 6)
+    return mtx, extrinsics
+
+
+def calibrate_camera(img_size, imgpoints, objpoints):
+    n = len(imgpoints)
+    assert n == len(objpoints)
+    # Compute homography matrix for each image
+    Hs = []
+    for i in range(n):
+        H = compute_homography(
+            objpoints[i][:, :2],
+            imgpoints[i].reshape(-1, 2),
+        )
+        Hs.append(H)
+
+    K = compute_intrinsic_matrix(Hs)
+    K = np.array(
+        [
+            [3180.23295976, 0.0, 1639.78082629],
+            [0.0, 3200.46103117, 1428.06578153],
+            [0.0, 0.0, 1.0],
+        ]
+    )
+    extrinsics = []
+    for H in Hs:
+        extrinsics.append(compute_extrinsic_matrix(K, H))
+    extrinsics = np.array(extrinsics)
+    ic(extrinsics)
+    return K, extrinsics
+
+
+def load_images(images):
+    # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+    # (8,6) is for the given testing images.
+    # If you use the another data (e.g. pictures you take by your smartphone),
+    # you need to set the corresponding numbers.
+    corner_x = 7
+    corner_y = 7
+    objp = np.zeros((corner_x * corner_y, 3), np.float32)
+    objp[:, :2] = np.mgrid[0:corner_x, 0:corner_y].T.reshape(-1, 2)
+
+    # Arrays to store object points and image points from all the images.
+    objpoints = []  # 3d points in real world space
+    imgpoints = []  # 2d points in image plane.
+
+    # Step through the list and search for chessboard corners
+    print("Start finding chessboard corners...")
+    for idx, fname in enumerate(images):
+        img = cv2.imread(fname)
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        # plt.imshow(gray)
+
+        # Find the chessboard corners
+        print("find the chessboard corners of", fname)
+        ret, corners = cv2.findChessboardCorners(gray, (corner_x, corner_y), None)
+
+        # If found, add object points, image points
+        if ret == True:
+            objpoints.append(objp)
+            imgpoints.append(corners)
+
+            # Draw and display the corners
+            cv2.drawChessboardCorners(img, (corner_x, corner_y), corners, ret)
+            # plt.imshow(img)
+    return imgpoints, objpoints
+
+
+def plot_extrinsics(mtx, extrinsics):
+    # plot setting
+    # You can modify it for better visualization
+    fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
+
+    # display
+    # True -> fix board, moving cameras
+    # False -> fix camera, moving boards
+    min_values, max_values = show.draw_camera_boards(
+        ax=ax,
+        # camera setting
+        camera_matrix=mtx,
+        cam_width=0.064 / 0.1,
+        cam_height=0.032 / 0.1,
+        scale_focal=1600,
+        extrinsics=extrinsics,
+        # chess board setting
+        board_width=8,
+        board_height=6,
+        square_size=1,
+        # display
+        patternCentric=True,
+    )
+    X_min = min_values[0]
+    X_max = max_values[0]
+    Y_min = min_values[1]
+    Y_max = max_values[1]
+    Z_min = min_values[2]
+    Z_max = max_values[2]
+    ic(X_min, X_max, Y_min, Y_max, Z_min, Z_max)
+
+    max_range = np.array([X_max - X_min, Y_max - Y_min, Z_max - Z_min]).max() / 2.0
+
+    mid_x = (X_max + X_min) * 0.5
+    mid_y = (Y_max + Y_min) * 0.5
+    mid_z = (Z_max + Z_min) * 0.5
+
+    ax.set_xlim(mid_x - max_range, mid_x + max_range)
+    ax.set_ylim(mid_y - max_range, 0)
+    ax.set_zlim(mid_z - max_range, mid_z + max_range)
+
+    ax.set_xlabel("x")
+    ax.set_ylabel("z")
+    ax.set_zlabel("-y")
+    ax.set_title("Extrinsic Parameters Visualization")
+    # animation for rotating plot
+
+    # for angle in range(0, 360):
+    #     ax.view_init(30, angle)
+    #     plt.draw()
+    #     plt.pause(0.001)
+
+
+def main():
+    # Make a list of calibration images
+    images = glob.glob("data/*.jpg")
+    imgpoints, objpoints = load_images(images)
+
+    #######################################################################################################
+    #                                Homework 1 Camera Calibration                                        #
+    #               You need to implement camera calibration(02-camera p.76-80) here.                     #
+    #   DO NOT use the function directly, you need to write your own calibration function from scratch.   #
+    #                                          H I N T                                                    #
+    #                        1.Use the points in each images to find Hi                                   #
+    #                        2.Use Hi to find out the intrinsic matrix K                                  #
+    #                        3.Find out the extrensics matrix of each images.                             #
+    #######################################################################################################
+
+    print("Camera calibration...")
+    img_size = cv2.imread(images[0])[0].shape
+    mtx, extrinsics = calibrate_camera(img_size, imgpoints, objpoints)
+    # mtx, extrinsics = cv2_example(img_size, imgpoints, objpoints)
+
+    # show the camera extrinsics
+    print("Show the camera extrinsics")
+    plot_extrinsics(mtx, extrinsics)
+
+
+if __name__ == "__main__":
+    main()
---
+++ hw1/utils.py
@@ -0,0 +1,59 @@
+import numpy as np
+
+
+def compute_homography(pts_src, pts_dst):
+    A = []
+    for i in range(len(pts_src)):
+        x, y = pts_src[i]
+        u, v = pts_dst[i]
+        A.append([-x, -y, -1, 0, 0, 0, u * x, u * y, u])
+        A.append([0, 0, 0, -x, -y, -1, v * x, v * y, v])
+    A = np.array(A)
+    assert A.shape == (len(pts_src) * 2, 9)
+    U, S, V = np.linalg.svd(A)
+    H = V[-1].reshape(3, 3)
+    return H / H[2, 2]
+
+
+def compute_v(H, i, j):
+    return np.array(
+        [
+            H[0, i] * H[0, j],
+            H[0, i] * H[1, j] + H[1, i] * H[0, j],
+            H[1, i] * H[1, j],
+            H[2, i] * H[0, j] + H[0, i] * H[2, j],
+            H[2, i] * H[1, j] + H[1, i] * H[2, j],
+            H[2, i] * H[2, j],
+        ]
+    )
+
+
+def compute_intrinsic_matrix(Hs):
+    A = []
+    for H in Hs:
+        A.append(compute_v(H, 0, 1))
+        A.append(compute_v(H, 0, 0) - compute_v(H, 1, 1))
+    A = np.array(A)
+    U, S, V = np.linalg.svd(A)
+    v = V[-1]
+    B = np.array(
+        [
+            [v[0], v[1], v[3]],
+            [v[1], v[2], v[4]],
+            [v[3], v[4], v[5]],
+        ]
+    )
+    K = np.linalg.cholesky(B)
+    K = np.linalg.inv(K).T
+    return K / K[2, 2]
+
+
+def compute_extrinsic_matrix(K, H):
+    K_inv = np.linalg.inv(K)
+    r1, r2, t = np.dot(K_inv, H).T
+    t /= (np.linalg.norm(r1) + np.linalg.norm(r2)) / 2
+    r1 /= np.linalg.norm(r1)
+    r2 /= np.linalg.norm(r2)
+    r3 = np.cross(r1, r2)
+    R = np.array([r1, r2, r3]).T
+    return np.concatenate((R, t[:, None]), axis=1)
